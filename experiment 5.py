db222741ee4a41c1301b564ea38f2ffba898e080
revanth2732 <chunturoyal85@gmail.com>
2025-09-12 13:31:37 +0530

    Delete experiment 5.py

--- experiment 5.py
+++
@@ -1,8 +0,0 @@
-def find_machine_epsilon():
-    epsilon = 1.0
-    while (1.0 + epsilon) != 1.0:
-        epsilon /= 2.0
-    return epsilon * 2.0   # The last valid epsilon before it became 0
-if __name__ == "__main__":
-    result = find_machine_epsilon()
-    print("Machine Epsilon =", result)
